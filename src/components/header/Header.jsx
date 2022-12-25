474f7e5a9c777bc283494096e01ae45deae94de8
technoprogramming92 <technoprogramming92@gmail.com>
2022-12-25 11:23:56 +0530

    commit after making header and its components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { AppBar, Toolbar, styled, Box, Typography } from '@mui/material';
+
+//components
+import Search from './Search';
+import CustomButtons from './CustomButtons';
+
+const StyledHeader = styled(AppBar) `
+    background: #2874f0;  
+    height: 55px;  
+`
+
+const Component = styled(Box) `
+    margin-left: 12%;
+    line-height: 0;
+`
+const SubHeading = styled(Typography) `
+    font-size: 10px;
+    font-style: italic;
+`
+const PlusImage = styled('img')({
+    width: 10,
+    height: 10,
+    marginLeft: 4
+});
+
+const CustomButtonWrapper = styled(Box)`
+    margin: 0 5% 0 auto;
+`
+
+export default function Header() {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+
+
+  return (
+    <StyledHeader>
+        <Toolbar style={{ minHeight: 55 }}>
+            <Component>
+                <img src={ logoURL } alt="logo" style={{ width: 75 }} />
+                <Box style={{ display: 'flex' }}>
+                    <SubHeading>Explore&nbsp;
+                        <Box component="span" style={{ color: '#FFF500' }}>Plus</Box>
+                    </SubHeading>
+                    <PlusImage src={ subURL } alt="plus" />
+                </Box>
+            </Component>
+            <Search/>
+            <CustomButtonWrapper>
+                <CustomButtons/>
+            </CustomButtonWrapper>
+        </Toolbar>
+    </StyledHeader>
+  )
+}
